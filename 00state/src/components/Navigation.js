0b9421e90ded5ca5baf37dba54e2fd2e7d6b7e55
Yazid <yalhujery01@gmail.com>
2022-04-07 13:08:48 -0400

    push

--- 00state/src/components/Navigation.js
+++ 00state/src/components/Navigation.js
@@ -1,10 +1,11 @@
 import { Nav } from "react-bootstrap"
+import BasicButtons from "./LoginBtn"
 function Navigation(props) {
     return(
     <Nav>
-     <Nav.Item className={props.navPage === "Portfolio" ? "selectednav" : null} onClick={() =>{props.setNavPage("Portfolio")}  } >Portfolio</Nav.Item>
-     <Nav.Item className={props.navPage === "Resume" ? "selectednav" : null} onClick={() =>{props.setNavPage("Resume")} } >Resume</Nav.Item>
-     <Nav.Item className={props.navPage === "AboutMe" ? "selectednav" : null} onClick={() =>{props.setNavPage("AboutMe")}} >About Me</Nav.Item>
+    <BasicButtons />
+    <BasicButtons />
+    <BasicButtons />
     </Nav>
     
     
--- 00state/src/components/ProjectPiece.js
+++ 00state/src/components/ProjectPiece.js
@@ -6,7 +6,10 @@ return(
 <h3>{props.title}</h3>
 
 <a href={props.deployedlink ? props.deployedlink : props.githublink}> <img src={props.image} /> </a>
- 
+ {/* if this rep exits, then "name, title, party, phone number email"  else null
+  if this rep and photo exist then photo else null
+ if this rep and twitter exist then twitter else null.
+ if this rep and facebook exist then facebook else null. */}
 </div>
     )
 }
