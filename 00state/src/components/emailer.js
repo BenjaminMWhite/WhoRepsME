4159803e95a95c35f2178186c5289d575068d58b
ian <ian.7.shaw@gmail.com>
2022-04-08 14:01:32 -0400

    pull

--- 00state/src/components/emailer.js
+++ 00state/src/components/emailer.js
@@ -1,4 +1,7 @@
-
+const express = require("express");
+const nodemailer = require("nodemailer");
+const app = express();
+//require("dotenv").config();
 // boilerplate for node mailer
 
 let transporter = nodemailer.createTransport({
@@ -10,15 +13,31 @@ let transporter = nodemailer.createTransport({
       clientId: process.env.OAUTH_CLIENTID,
       clientSecret: process.env.OAUTH_CLIENT_SECRET,
       refreshToken: process.env.OAUTH_REFRESH_TOKEN
-    }
+    },
   });
 
 
   // this is our sending template
 
-  //const sendMail = {
-    //from: ourgooglecloud@gmail.com,
-   // to: concernedpoliticlady@gmail.com,
-   // subject: 'Election upcoming',
-   // text: 'Here is an update of your next election it will be on 6/24/2022'
- // };
+  const theEmail = {
+    from: ourgooglecloud@gmail.com,
+    to: concernedpoliticlady@gmail.com,
+    subject: 'Election upcoming',
+    text: 'Here is an update of your next election it will be on 6/24/2022'
+  };
+
+ 
+transporter.sendMail(theEmail), function (err, data) {
+    if (err) {
+      console.log("Error " + err);
+    } else {
+      console.log("Email sent successfully");
+    }
+   };
+
+   
+//const port = 3001;
+//app.listen(port, () => {
+// console.log(`Server is running on port: ${port}`);
+//});
+   
