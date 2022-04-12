de3edd95983ad57f4f98f707507d701cfb42574a
Yazid <yalhujery01@gmail.com>
2022-04-12 10:26:39 -0400

    changed connection file

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,9 +1,18 @@
 const mongoose = require('mongoose');
 
-const connection_string = process.env.CONNECTION_STRING || 'mongodb://127.0.0.1:27017/WhoRepsME'
+// const connection_string = process.env.CONNECTION_STRING || 'mongodb://127.0.0.1:27017/WhoRepsME'
 
-mongoose.connect(connection_string)
-    .then(() => console.log('mongoDB connection IS working '))
-    .catch((error) => console.error('MongoDB not working up!:', error.message));
+// mongoose.connect(connection_string)
+//     .then(() => console.log('mongoDB connection IS working '))
+//     .catch((error) => console.error('MongoDB not working up!:', error.message));
+
+mongoose.connect(
+    process.env.MONGODB_URI || 'mongodb://localhost:27017/WhoRepsME',
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    },
+  );
+  
 
 module.exports = mongoose.connection;
